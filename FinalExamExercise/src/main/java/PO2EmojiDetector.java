0f2bbc9ebccc2860537004404804f2c35eaa8526
Alexandar Georgiev <georgievalex807@gmail.com>
2025-03-23 21:12:20 +0200

    feat:

--- FinalExamExercise/src/main/java/PO2EmojiDetector.java
+++ FinalExamExercise/src/main/java/PO2EmojiDetector.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PO2EmojiDetector {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         String text=scanner.nextLine();
+
+        Pattern pattern=Pattern.compile("(:{2}|\\*{2})[A-Z][a-z]{2,}\\1");
+        Pattern patternNum=Pattern.compile("\\d");
+        Matcher matcherNum= patternNum.matcher(text);
+        Matcher matcher= pattern.matcher(text);
+        List<String>emojis=new ArrayList<>();
+       long coolTresHold=1;
+        while (matcherNum.find()){
+            coolTresHold*=Integer.parseInt(matcherNum.group());
+        }
+        while (matcher.find()){
+            emojis.add(matcher.group());
+        }
+
+        List<String>emoji3=new ArrayList<>();
+        for (String emoji : emojis) {
+            int sum = 0;
+            for (int j = 2; j < emoji.length()-2; j++) {
+                sum += emoji.charAt(j);
+
+            }
+            if (sum >= coolTresHold) {
+                emoji3.add(emoji);
+
+            }
+        }
+
+        System.out.println("Cool threshold: "+coolTresHold);
+        System.out.printf("%d emojis found in the text. The cool ones are:\n ",emojis.size());
+        for (String string : emoji3) {
+            System.out.println(string);
+        }
+
+    }
 }
--- FinalExamExercise/src/main/java/PO3PlantDiscovery.java
+++ FinalExamExercise/src/main/java/PO3PlantDiscovery.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class PO3PlantDiscovery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, Integer> plantsMap = new LinkedHashMap<>();
+        Map<String, List<Double>> ratingMap = new LinkedHashMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+            String[] text = scanner.nextLine().split("<->");
+            String flower = text[0];
+            int rarity = Integer.parseInt(text[1]);
+            plantsMap.put(flower, rarity);
+
+            ratingMap.putIfAbsent(flower, new ArrayList<>());
+        }
+        String command = scanner.nextLine();
+        while (!command.equals("Exhibition")) {
+            String commandName = command.split(": ")[0];
+            String[] commandParts = command.split(": ");
+            String plant = commandParts[1].split(" - ")[0];
+            if (!ratingMap.containsKey(plant) || !plantsMap.containsKey(plant)) {
+                System.out.println("error");
+                command = scanner.nextLine();
+
+            } else {
+
+
+                if (commandName.equals("Rate")) {
+                    double rating = Double.parseDouble(commandParts[1].split(" - ")[1]);
+                    ratingMap.get(plant).add(rating);
+                } else if (commandName.equals("Update")) {
+                    int newRarity= Integer.parseInt(commandParts[1].split(" - ")[1]);
+                    plantsMap.put(plant,newRarity);
+                    
+                }else if(commandName.equals("Reset")){
+
+                    ratingMap.get(plant).clear();
+                }
+
+
+
+                command = scanner.nextLine();
+            }
+        }
+        System.out.println("Plants for the exhibition:");
+        plantsMap.entrySet().forEach(entry->{
+            String name= entry.getKey();
+           int rarity=entry.getValue();
+           double avgRating=ratingMap.get(name).stream().mapToDouble(rating->rating).average().orElse(0);
+            System.out.printf("- %s; Rarity: %d; Rating: %.2f\n",name,rarity,avgRating);
+
+        });
+
+
+    }
 }
+//•	"Rate: {plant} - {rating}" – add the given rating to the plant (store all ratings)
+//•	"Update: {plant} - {new_rarity}" – update the rarity of the plant with the new one
+//•	"Reset: {plant}" – remove all the ratinggs of the given plant
